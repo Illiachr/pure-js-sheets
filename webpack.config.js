75ed21419a99ae77757a0858c83826cc3d9a47d1
Illiachr <74302872+Illiachr@users.noreply.github.com>
2021-02-16 00:15:20 +0200

    feat: add babel-polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const path = require('path');
 module.exports = {
   context: path.resolve(__dirname, 'src'),
   entry: {
-    main: './index.js'
+    main: ['@babel/polyfill','./index.js']
   },
   output: {
     path: path.resolve(__dirname, 'build'),
