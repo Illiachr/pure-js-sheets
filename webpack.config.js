a9b5bab5510319d1379e2d8a1cab082b7ee10ce3
Illiachr <74302872+Illiachr@users.noreply.github.com>
2021-02-15 23:39:31 +0200

    feat: add plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const copyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -8,5 +10,26 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: '[name].bundle.js',
-  }
+  },
+  resolve: {
+    extnsions: ['.js'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@core': path.resolve(__dirname, 'src', 'core'),
+    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+    new copyWebpackPlugin({
+      patterns: [
+        { 
+          from: path.resolve(__dirname, 'src', 'img'),
+          to: path.resolve(__dirname, 'build', 'img'), 
+        },
+        
+      ],
+    }),
+  ],
 }
