415c2634c9917323f25ffab5efc0ec5430b849b7
Illiachr <74302872+Illiachr@users.noreply.github.com>
2021-02-17 13:39:03 +0200

    feat: add ESLint plugin, dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,90 @@
-const copyWebpackPlugin = require('copy-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const ESLintWebpackPlugin = require('eslint-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
-module.exports = {
-  context: path.resolve(__dirname, 'src'),
-  entry: {
-    main: ['@babel/polyfill','./index.js']
-  },
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: '[name].bundle.js',
-  },
-  resolve: {
-    extensions: ['.js'],
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '@core': path.resolve(__dirname, 'src', 'core'),
-    }
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html',
-    }),
-    new copyWebpackPlugin({
-      patterns: [
-        { 
-          from: path.resolve(__dirname, 'src', 'img'),
-          to: path.resolve(__dirname, 'build', 'img'), 
-        },
-        
-      ],
-    }),
-    new MiniCssExtractPlugin({
-      filename: '[name].bundle.css',
-    }),
-  ],
-  module: {
-    rules: [
-    {
-      test: /\.s[ac]ss$/i,
-        use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader",
-          "sass-loader",
+module.exports = (env, argv) => {
+  const isProd = argv.mode === 'production';
+  const isDev = !isProd;
+
+  const filename = (ext) =>
+    isProd ? `[name].[contenthash].bundle.${ext}` : `[name].bundle.${ext}`;
+
+  const plugins = () => {
+    const base = [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+      }),
+      new CopyPlugin({
+        patterns: [
+          {
+            from: path.resolve(__dirname, 'src', 'img'),
+            to: path.resolve(__dirname, 'build', 'img'),
+          },
+
         ],
+      }),
+      new MiniCssExtractPlugin({
+        filename: `./css/${filename('css')}`,
+      }),
+      new CleanWebpackPlugin()
+    ];
+
+    if (isDev) {
+      base.push(new ESLintWebpackPlugin());
+    }
+
+    return base;
+  };
+
+  return {
+    target: 'web',
+    context: path.resolve(__dirname, 'src'),
+    entry: {
+      main: ['@babel/polyfill', './index.js']
     },
-      {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ['@babel/preset-env']
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: `./js/${filename('js')}`,
+    },
+    resolve: {
+      extensions: ['.js'],
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+        '@core': path.resolve(__dirname, 'src', 'core'),
+      }
+    },
+    devServer: {
+      port: '3000',
+      open: true,
+      // hot: true,
+      watchContentBase: true,
+    },
+    plugins: plugins(),
+    devtool: isDev ? 'source-map' : false,
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            'sass-loader',
+          ],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
           }
-        }
-      },
-    ]
-  },
-}
+        },
+      ]
+    },
+  };
+};
