ba64c8d973aa0b7155b7de83d858309fee8d3ea4
Illiachr <74302872+Illiachr@users.noreply.github.com>
2021-02-21 20:14:25 +0200

    delete unnecessary import from table.js

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,5 +1,5 @@
 import SheetComponent from '@core/SheetComponent';
-import {$} from '@core/dom';
+import {resizeHandler} from './handlers';
 import {createTable} from './tableTemplate';
 
 export default class Table extends SheetComponent {
@@ -26,51 +26,3 @@ export default class Table extends SheetComponent {
     } // end if resize
   }
 }
-
-const resizeHandler = ($root, $target) => {
-  const $resizer = $($target);
-  const $parent = $resizer.closest('[data-type="resizable"]');
-  const coords = $parent.getCoordinats();
-  const direction = $resizer.data.resize;
-  const sideProp = direction === 'col' ? 'bottom': 'right';
-
-  $resizer.css({
-    opacity: 1,
-    [sideProp]: '-5000px'
-  });
-
-  const colIndex = direction === 'col' ? $parent.data.col : null;
-  const $cells = direction === 'col'
-              ? $root.querySelectorAll(`[data-col="${colIndex}"]`)
-              : null;
-  let value = '';
-
-  document.onmousemove = e => {
-    if (direction === 'col') {
-      const delta = e.pageX - coords.right;
-      value = `${coords.width + delta}px`;
-      $resizer.css({transform: `translateX(${delta}px)`});
-    } else {
-      const delta = e.pageY - coords.bottom;
-      value = `${coords.height + delta}px`;
-      $resizer.css({transform: `translateY(${delta}px)`});
-    }
-  };
-
-  document.onmouseup = () => {
-    document.onmousemove = null;
-    document.onmouseup = null;
-    $resizer.css({
-      opacity: 0,
-      [sideProp]: 0,
-      transform: `translate(0, 0)`
-    });
-    if (direction === 'col') {
-      $cells.forEach(cell => {
-        cell.style.cssText = `width: ${value}`;
-      });
-    } else {
-      $parent.css({height: value});
-    }
-  };
-}; // end resizeHandler
