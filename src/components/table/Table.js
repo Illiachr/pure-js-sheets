089e1de1319861671c9ccb9239704259dc602a0b
Illiachr <74302872+Illiachr@users.noreply.github.com>
2021-02-22 22:38:04 +0200

    was added key navigation, todo: modify to better implementation

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -11,7 +11,7 @@ export default class Table extends SheetComponent {
   constructor($root) {
     super($root, {
       name: Table,
-      listeners: ['mousedown'],
+      listeners: ['mousedown', 'keydown'],
     });
   }
 
@@ -45,6 +45,49 @@ export default class Table extends SheetComponent {
     }
     if (isResizable(event)) {
       resizeHandler(this.$root, event.target);
+    }
+  }
+
+  onKeydown(event) {
+    event.preventDefault();
+    if (isCell(event)) {
+      const {keyCode} = event;
+      const currCell = $(event.target);
+      const id = currCell.id(true);
+      let currRow = id.row;
+      let currCol = id.col;
+
+      switch (keyCode) {
+        case (keyCode === 13 || keyCode === 40):
+          console.log('go next down');
+          break;
+      }
+
+      if (keyCode === 13 || keyCode === 40) {
+        currRow += 1;
+      }
+
+      if (keyCode === 38) {
+        console.log('go next up');
+
+        currRow -= 1;
+      }
+
+      if (keyCode === 9 || keyCode === 39) {
+        console.log('go next right');
+        currCol += 1;
+      }
+
+      if (keyCode === 37) {
+        console.log('go next left');
+        currCol -= 1;
+      }
+
+      const $targetCell = this.$root
+          .find(`[data-id="${currRow}:${currCol}"]`);
+      if ($targetCell.$el) {
+        this.tableSelection.select($targetCell);
+      }
     }
   }
 }
